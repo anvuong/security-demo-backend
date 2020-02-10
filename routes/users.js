e7b09ce800bdfa4b3c7801575db5ce3318a231d8
Vuong Hong An <vuong_hong_an@i-hayabusa.com>
2020-02-10 13:50:58 +0700

    API to get user by id

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,4 @@
+const { QueryTypes } = require('sequelize');
 var models  = require('../models');
 var express = require('express');
 var router  = express.Router();
@@ -18,6 +19,64 @@ router.get('/:user_id/destroy', function(req, res) {
   }).then(function() {
     res.redirect('/');
   });
+});
+
+router.get('/:user_id', function(req, res, next) {
+  models.sequelize.query('SELECT username, u.createdAt FROM `Users` u WHERE id = \'' + req.params.user_id + '\'', {
+    type: QueryTypes.SELECT,
+  }).then(function(users) {
+    const user = Array.isArray(users) && users.length > 0 ? users[0] : null;
+    if (user) {
+      res.json(user);
+    } else {
+      res.send('No user found!');
+    }
+  }).catch(function(error) {
+    next(error);
+  });
+
+  // Prevent SQL injection by escaping user's input before inserting it into the query. (using "replacements" in sequelize)
+  // ※ https://sequelize.org/master/manual/raw-queries.html
+  // models.sequelize.query('SELECT username, u.createdAt FROM `Users` u WHERE id = :userId', {
+  //   replacements: { userId: req.params.user_id },
+  //   type: QueryTypes.SELECT,
+  // }).then(function(users) {
+  //   const user = Array.isArray(users) && users.length > 0 ? users[0] : null;
+  //   if (user) {
+  //     res.json(user);
+  //   } else {
+  //     res.send('No user found!');
+  //   }
+  // }).catch(function(error) {
+  //   next(error);
+  // });
+
+  // Prevent SQL injection by using prepared statement. (using "bind" in sequelize)
+  // ※ https://sequelize.org/master/manual/raw-queries.html
+  // models.sequelize.query('SELECT username, u.createdAt FROM `Users` u WHERE id = $1', {
+  //   bind: [req.params.user_id],
+  //   type: QueryTypes.SELECT,
+  // }).then(function(users) {
+  //   const user = Array.isArray(users) && users.length > 0 ? users[0] : null;
+  //   if (user) {
+  //     res.json(user);
+  //   } else {
+  //     res.send('No user found!');
+  //   }
+  // }).catch(function(error) {
+  //   next(error);
+  // });
+
+  // Prevent SQL injection by using data mapper pattern, without writing any SQL codes
+  // models.User.findByPk(req.params.user_id, {
+  //   attributes: ['username', 'createdAt']
+  // }).then(user => {
+  //   if (user) {
+  //     res.json(user);
+  //   } else {
+  //     res.send('No user found!');
+  //   }
+  // });
 });
 
 router.post('/:user_id/tasks/create', function (req, res) {
