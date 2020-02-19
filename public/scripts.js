9434b57f54cbfcebdea99e8a4d27dae14a6487f3
Vuong Hong An <anvh87@gmail.com>
2020-02-19 08:27:09 +0700

    Store & get data from localStorage

--- public/scripts.js
+++ public/scripts.js
@@ -1,5 +1,12 @@
-const func = () => {
-  alert('This is the dialog called by "func()"');
+const readLocalStorage = () => {
+  const storageInfos = [];
+  const storageLength = localStorage.length;
+  for (let i = 0; i < storageLength; i += 1) {
+    const key = localStorage.key(i);
+    const value = localStorage.getItem(key);
+    storageInfos.push({ key, value });
+  }
+  alert(`${JSON.stringify(storageInfos, null, 2)}`);
 };
 
-func();
+readLocalStorage();
